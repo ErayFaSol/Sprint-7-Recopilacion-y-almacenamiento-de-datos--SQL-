5d194f8db1a3bffa0b2e1514868e0356a559bbca
Eduardo Franco <eduardo.fsolorzano@alumnos.udg.mx>
2024-09-14 16:42:22 -0600

    report ajust

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 from preprocessing.preprocessing import load_data, inspect_data, clean_data
 from eda.eda import plot_top_10_taxi_company, plot_top_10_dropoff
 from hypothesis_testing.hypothesis_testing import hypothesis_test
-from utils.generate_report import save_report_as_html
+from utils.generate_report import save_report
 
 # Cargar los datos
 df1, df2, df3 = load_data()
@@ -24,6 +24,28 @@ plot_top_10_dropoff(df2, 'dropoff_locations.png')
 # Prueba de hipótesis
 good_weather = df3[df3['weather_conditions'] == 'Good']['duration_seconds']
 bad_weather = df3[df3['weather_conditions'] == 'Bad']['duration_seconds']
-hypothesis_test(good_weather, bad_weather)
+pvalue_hypothesis = hypothesis_test(good_weather, bad_weather)
+
+# Contenido de reporte
+report_content = """
+<h1>Reporte Final de Analisis</h1>
+<h2>Grafico: Numero de Viajes por Empresa de Taxis</h2>
+<img src="images/taxi_company.png" alt="Grafico de Empresas de Taxis y Numero de Viajes">
+
+<h2>Grafico: Top 10 Barrios por Promedio de Finalizacion de Viajes</h2>
+<img src="images/dropoff_locations.png" alt="Grafico de Top 10 Barrios por Promedio de Finalizacion">
+
+<h2>Resultados de las Pruebas de Hipótesis</h2>
+<p><b>Hipótesis 1:</b> La duración promedio de los viajes desde el Loop hasta el Aeropuerto Internacional O'Hare NO cambia los sábados lluviosos.</p>
+<p>Resultado de la prueba: p-value = {pvalue_hypothesis}</p>
+
+<h2>Conclusiones</h2>")
+<p>El analisis mostro que Flash Cab es la empresa con mayor numero de viajes, y Loop es el barrio con mas finalizaciones de viajes.</p>")
+
+"""
+report_content = report_content.format(
+    pvalue_hypothesis = pvalue_hypothesis
+)
+
 # Generar el informe final en HTML
-save_report_as_html()
+save_report(report_content)
--- src/utils/generate_report.py
+++ src/utils/generate_report.py
@@ -1,17 +1,5 @@
 # generate_report.py
-def save_report_as_html():
-    with open("reporte_final.html", "w") as file:
-        # Título del reporte
-        file.write(f"<h1>Reporte Final de Analisis</h1>")
-        
-        # Grafica 1: Empresas de taxi y numero de viajes
-        file.write(f"<h2>Grafico: Numero de Viajes por Empresa de Taxis</h2>")
-        file.write(f'<img src="images/taxi_company.png" alt="Grafico de Empresas de Taxis y Numero de Viajes">')
-
-        # Grafica 2: Barrios por Promedio de Finalizacion de Viajes
-        file.write(f"<h2>Grafico: Top 10 Barrios por Promedio de Finalizacion de Viajes</h2>")
-        file.write(f'<img src="images/dropoff_locations.png" alt="Grafico de Top 10 Barrios por Promedio de Finalizacion">')
-
-        # Conclusiones
-        file.write(f"<h2>Conclusiones</h2>")
-        file.write(f"<p>El analisis mostro que Flash Cab es la empresa con mayor numero de viajes, y Loop es el barrio con mas finalizaciones de viajes.</p>")
+def save_report(analysis_results, filepath="final_report.html"):
+    with open(filepath, 'w') as file:
+        file.write("<h1>Reporte Final</h1>")
+        file.write(f"<p>{analysis_results}</p>")
